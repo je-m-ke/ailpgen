3dbed36383ebd8e6a7daa697fb15bdea90abc176
Jmk <JmK@Apples-MacBook-Pro.local>
2026-05-16 13:59:27 +0530

    fixed supabase project url

--- supabase_client.py
+++ supabase_client.py
@@ -1,6 +1,6 @@
 from supabase import create_client
 
-url = "https://artjvcoqftopdcwgffsm.supabase.co/rest/v1/"
+url = "https://artjvcoqftopdcwgffsm.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFydGp2Y29xZnRvcGRjd2dmZnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5MTQ1MTcsImV4cCI6MjA5NDQ5MDUxN30.FEZt7tUR0TxXccc943FFkdkoe_1PwI_Ku7h68mOhzyM"
 
 supabase = create_client(url, key)
